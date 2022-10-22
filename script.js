db98111ebd470f13ecb23008958e6f68ffd1898e
amin <amin.haghi@hyperisland.se>
2022-10-22 16:19:27 +0200

    add some effects

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
 "use strict";
 
-//  DECLARE BUTTON 
+//  DECLARE variables
 let button = document.getElementById("btn");
-
+let res = document.getElementById("result");
+let win = document.getElementById("win");
+let resetPage = document.getElementById("reset");
+let Realnumbers = Math.floor(Math.random() * 100 + 1);
+console.log(Realnumbers);
+win.style.display="none"
 // LISTEN TO CLICK EVENT
 button.addEventListener("click", runGame);
 
 // RUN THE GAME BY PRESING BUTTON
 function runGame() {
-  let Realnumbers = Math.floor(Math.random() * 100 + 1);
-  let i = 1;
-  while (i < 11) {
-    let guessNumber = Number(
-      prompt("Please Guess the number between 1 to 100")
-    );
+  let guessNumber = Number(prompt("Please Guess the number between 1 to 100"));
+  if (guessNumber === Realnumbers) {
 
-    if (guessNumber === Realnumbers) {
-      alert("woooooooooo!!!!!You Win");
-      break;
-    } else {
-      i++;
-    }
+    win.style.display="block"
+    button.style.visibility = "hidden";
+    console.log("woooooooooo!!!!!You Win");
+    res.style.visibility='hidden'
+    win.innerHTML = "woooow!!!You guess right!";
+    win.style.color='green'
+    resetPage.style.display = "block";
+  } else if (guessNumber < Realnumbers) {
+    res.innerHTML = "you gues smaller number";
+    res.style.color='yellow'
+  } else if (guessNumber > Realnumbers) {
+    res.innerHTML = "you gues larger number";
+    res.style.color='red'
   }
 }
+
+resetPage.addEventListener("click", () => {
+  location.reload();
+});
