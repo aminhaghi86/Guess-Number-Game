7317683351c5f5f2cc3cec85ded08d41933d6571
Amin.Haghi <90243818+AminFED23STO@users.noreply.github.com>
2022-10-23 22:38:52 +0200

    fix some bugs

--- script.js
+++ script.js
@@ -5,15 +5,15 @@ let button = document.getElementById("btn");
 let res = document.getElementById("result");
 let win = document.getElementById("win");
 let resetPage = document.getElementById("reset");
-let Realnumbers = Math.floor(Math.random() * 100 + 1);
+let Realnumber = Math.floor(Math.random() * 100 + 1);
 win.style.display="none"
 // LISTEN TO CLICK EVENT
 button.addEventListener("click", runGame);
-
+console.log(Realnumber);
 // RUN THE GAME BY PRESING BUTTON
 function runGame() {
   let guessNumber = Number(prompt("Please Guess the number between 1 to 100"));
-  if (guessNumber === Realnumbers) {
+  if (guessNumber === Realnumber) {
     win.style.display="block"
     button.style.visibility = "hidden";
     console.log("woooooooooo!!!!!You Win");
@@ -21,10 +21,10 @@ function runGame() {
     win.innerHTML = "woooow!!!You guess right!";
     win.style.color='green'
     resetPage.style.display = "block";
-  } else if (guessNumber < Realnumbers) {
+  } else if (guessNumber < Realnumber) {
     res.innerHTML = "you guess smaller number";
     res.style.color='yellow'
-  } else if (guessNumber > Realnumbers) {
+  } else if (guessNumber > Realnumber) {
     res.innerHTML = "you gues larger number";
     res.style.color='red'
   }
