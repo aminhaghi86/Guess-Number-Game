6db42b590f34443d2f3780b22a5ef2dbc5604db1
amin <amin.haghi@hyperisland.se>
2022-10-28 17:54:45 +0200

    add date into project

--- script.js
+++ script.js
@@ -1,35 +1,48 @@
 "use strict";
-
 //  DECLARE variables
 let button = document.getElementById("btn");
 let res = document.getElementById("result");
 let win = document.getElementById("win");
 let resetPage = document.getElementById("reset");
-let Realnumber = Math.floor(Math.random() * 100 + 1);
-win.style.display="none"
+let timeDispay = document.getElementById("timeDispay");
+
+let originalNum = Math.floor(Math.random() * 100) + 1;
+win.style.display = "none";
 // LISTEN TO CLICK EVENT
 button.addEventListener("click", runGame);
-console.log(Realnumber);
+console.log(originalNum);
 // RUN THE GAME BY PRESING BUTTON
 function runGame() {
   let guessNumber = Number(prompt("Please Guess the number between 1 to 100"));
-  if (guessNumber === Realnumber) {
-    win.style.display="block"
+  if (guessNumber !=''){
+  if (guessNumber === originalNum) {
+    win.style.display = "block";
     button.style.visibility = "hidden";
-    console.log("woooooooooo!!!!!You Win");
-    res.style.visibility='hidden'
-    win.innerHTML = "woooow!!!You guess right!";
-    win.style.color='green'
+    res.style.visibility = "hidden";
+    win.innerHTML = ` Yes!!!!! The Number is ${guessNumber}`;
+    win.style.color = "green";
     resetPage.style.display = "block";
-  } else if (guessNumber < Realnumber) {
-    res.innerHTML = "you guess smaller number";
-    res.style.color='yellow'
-  } else if (guessNumber > Realnumber) {
-    res.innerHTML = "you gues larger number";
-    res.style.color='red'
+  } else if (guessNumber < originalNum) {
+    res.innerHTML = `${guessNumber} is less than original Number`;
+    res.style.color = "yellow";
+  } else if (guessNumber > originalNum) {
+    res.innerHTML = `${guessNumber} is greater than original Number`;
+    res.style.color = "red";
   }
+}else {
+  alert('please enter number :)')
 }
-
+}
 resetPage.addEventListener("click", () => {
   location.reload();
 });
+
+// display date
+let newdate = new Date();
+let today =
+  newdate.getFullYear() +
+  "-" +
+  (newdate.getMonth() + 1) +
+  "-" +
+  newdate.getDate();
+timeDispay.innerHTML += today;
